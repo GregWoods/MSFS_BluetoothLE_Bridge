93e23cbff8805ed699c0f2e121d6d026bea2af19
Greg Woods <gregwoodslancs@gmail.com>
2025-08-31 01:08:12 +0100

    config file is now .settings to keep VS intellisence happy

--- apps/msfs_ble_bridge_main.cpp
+++ apps/msfs_ble_bridge_main.cpp
@@ -12,11 +12,11 @@
 #include "simpleble/SimpleBLE.h"
 #include "WASMIF.h"
 
-// Command-line selectable config; defaults to "default.config" if not provided via --config
-constexpr const char* DEFAULT_CONFIG_FILENAME = "default.config";
+// Command-line selectable settings file; defaults to "default.settings" if not provided via --config
+constexpr const char* DEFAULT_CONFIG_FILENAME = "default.settings";
 constexpr const char* BLE_STRING_IDENTIFIER = "SHB1000";
 constexpr const char* BLE_CHARACTERISTIC_UUID = "f62a9f56-f29e-48a8-a317-47ee37a58999";
-constexpr int BLUETOOTH_SCANNING_TIMEOUT_SEC = 22;
+constexpr int BLUETOOTH_SCANNING_TIMEOUT_SEC = 27;
 
 static_assert(sizeof(void*) == 8, "Must build 64-bit (x64).");
 
